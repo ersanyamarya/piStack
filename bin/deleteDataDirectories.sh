26cc22f9a94cb3a1d30847fa3525575ebbcdab98
Sanyam Arya <er.sanyam.arya@gmail.com~>
2020-12-26 19:53:35 +0100

    updated nginx

--- bin/deleteDataDirectories.sh
+++ bin/deleteDataDirectories.sh
@@ -6,3 +6,5 @@ sudo rm -rf ./telegraf/data
 sudo rm -rf ./mosquitto/data
 sudo rm -rf ./mosquitto/log
 sudo rm -rf ./portainer/data
+sudo rm -rf ./nginx/certbot/conf
+sudo rm -rf ./nginx/certbot/www
